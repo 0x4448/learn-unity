840ce3d0c559206f57b2330a73a6592936bc065d
0x4448 <86135470+0x4448@users.noreply.github.com>
2022-07-31 19:44:28 -0700

    Subscribe to jump with C#

--- Assets/Input/Scripts/PlayerInput.cs
+++ Assets/Input/Scripts/PlayerInput.cs
@@ -9,14 +9,15 @@ public class PlayerInput : MonoBehaviour
     private void Awake()
     {
         player = GetComponent<Rigidbody>();
-        
+
+        // Generated C# class from input action asset
+        PlayerInputAction inputAction = new();
+        inputAction.OnFoot.Enable();
+        inputAction.OnFoot.Jump.performed += Jump;
     }
 
     public void Jump(InputAction.CallbackContext context)
     {
-        if (context.performed)
-        {
-            player.AddForce(Vector3.up * 5f, ForceMode.Impulse);
-        }
+        player.AddForce(Vector3.up * 5f, ForceMode.Impulse);
     }
 }
